f50d034f7e88db1ca118664b3eec86bbcd116986
notgaby <45638369+notgaby@users.noreply.github.com>
2020-01-02 17:48:25 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		// creates the frame
+		//System.out.println(new WumpusMap().toString());
+		new WumpusFrame("Wumpus",600,650);
+		//600,650
+	}
+}
